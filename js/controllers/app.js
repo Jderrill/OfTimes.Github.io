0e2a2f92980c6f51e25a24be036d475a69959927
Jderrill <jderrill@gmail.com>
2016-08-24 14:30:53 -0600

    Posts page, changed up home page, routes.

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -38,6 +38,38 @@ var postsData = {
 		So some things happened. So some things happened. \
 		So some things happened. So some things happened. "
 
+		},
+		"3" :{
+		"id" : "3",
+		"author" : "Cori",
+		"pic" : cori_pic,
+		"quote" : cori_quote,
+		"date" : "Aug 36, 2017",
+		"title" : "The Shish You DO Know",
+		"media" : [],
+		"summary" : "So no things happened.",
+		"post" : "So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. "
+		},
+		"4" :{
+		"id" : "4",
+		"author" : "Cori",
+		"pic" : cori_pic,
+		"quote" : cori_quote,
+		"date" : "Aug 36, 2017",
+		"title" : "Ahh Poo",
+		"media" : ["http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg"],
+		"summary" : "So no things happened.",
+		"post" : "So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. \
+		So some things happened. So some things happened. "
 	}
 	};
 
@@ -51,8 +83,12 @@ App.config(function($routeProvider){
 		templateUrl: '/partials/index.html',
 		controller: 'authorsController'
 	})
-	.when('/posts/:id',{
+	.when('/posts',{
 		templateUrl: '/partials/posts.html',
+		controller: 'postsPageController'
+	})
+	.when('/posts/:id',{
+		templateUrl: '/partials/post_template.html',
 		controller: 'postsController'
 	})
 	.otherwise({
@@ -61,7 +97,18 @@ App.config(function($routeProvider){
 });
 
 App.controller('authorsController', function($scope){
-	$scope.posts = postsData
+	$scope.allPosts = postsData
+	//get id of the newest post
+	newestId = Object.keys(postsData).length
+	$scope.newestPost = postsData[newestId]
+	$scope.posts = []
+
+
+	//last three posts to display
+	$scope.posts.push(postsData[newestId-1])
+	$scope.posts.push(postsData[newestId-2])
+	$scope.posts.push(postsData[newestId-3])
+
 });
 
 App.controller('postsController', function($scope,$routeParams){
@@ -71,3 +118,16 @@ App.controller('postsController', function($scope,$routeParams){
 	$scope.currentPost = postsData[currentId]
 });
 
+App.controller('postsPageController', function($scope){
+	newestId = Object.keys(postsData).length
+	$scope.posts12 = []
+	if (newestId <= 12){
+		var limit = newestId-1;
+	} else{
+		var limit = 11
+	}
+	for(var i=0; i <= limit; i++){
+		$scope.posts12.push(postsData[newestId-i]);
+	}
+	console.log($scope.posts12)
+});
